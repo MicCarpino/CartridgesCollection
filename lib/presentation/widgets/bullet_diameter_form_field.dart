ab67ad61eb2c108735da05ef30e54df3590beb69
DNC <michael.carpino@dncsrl.com>
2021-03-15 10:42:38 +0100

    Insert/Update method for form, deletion dialog

---
+++ lib/presentation/widgets/bullet_diameter_form_field.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_app/presentation/bloc/cartridge_form/cartridge_form_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class BulletDiameterFormField extends StatelessWidget {
+  final TextEditingController textEditingController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<CartridgeFormBloc, CartridgeFormState>(
+      listenWhen: (p, c) => p.isEditing != c.isEditing,
+      listener: (context, state) {
+        textEditingController.text = state.cartridge.bulletDiameter.toString();
+      },
+      child: TextFormField(
+        keyboardType: const TextInputType.numberWithOptions(decimal: true),
+        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+        controller: textEditingController,
+        decoration: const InputDecoration(labelText: 'Diametro palla'),
+        onChanged: (value) => context
+            .read<CartridgeFormBloc>()
+            .add(CartridgeFormEvent.bulletDiameterChanged(value)),
+        validator: (_) => context
+            .read<CartridgeFormBloc>()
+            .state
+            .cartridge
+            .bulletDiameter == 0.0
+            ? 'Non valido'
+            : null,
+      ),
+    );
+  }
+}
